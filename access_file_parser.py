da5a09166e3b454e74bf0e2c9fae6e537668e0c5
Div <28270726+divprasad@users.noreply.github.com>
2025-02-11 16:11:09 +0100

    clean parsing script

--- access_file_parser.py
+++ access_file_parser.py
@@ -2,66 +2,57 @@ import argparse
 
 parser = argparse.ArgumentParser(
                     prog='submission parser',
-                    description='GISAID/ENA submission parser from analysis_file')
+                    description='GISAID/ENA submission parser from access_file')
 
-parser.add_argument('filename', help="'dd-mm-yy' of the analysis_file")           # positional argument
+parser.add_argument('filename', help="'dd-mm-yy' of the access_file")
 
 args = parser.parse_args()
-#print(args.filename, args.count, args.verbose)
 
-datum=args.filename #"20-03-23"
+submission_date=args.filename #"20-03-23"
 
-# afile_in="/mnt/d/1.EMC/0.ENA/"+datum+".tsv"
-# gfasta_out="/mnt/d/1.EMC/0.ENA/GISAID_"+datum+".fasta"
-# gfiletsv_out="/mnt/d/1.EMC/0.ENA/gisaid_"+datum+".tsv"
-# linksf_out="/mnt/d/1.EMC/0.ENA/makeLinks_"+datum+".sh"
+afile_in=submission_date+".tsv"
+gfasta_out="GISAID_"+submission_date+".fasta"
+gfiletsv_out="gisaid_"+submission_date+".tsv"
+linksf_out="makeLinks_"+submission_date+".sh"
 
-afile_in=datum+".tsv"
-gfasta_out="GISAID_"+datum+".fasta"
-gfiletsv_out="gisaid_"+datum+".tsv"
-linksf_out="makeLinks_"+datum+".sh"
-
-count=0
-tosubmit=0
-first_alias=[]
+seqs_in_access_file=0
+seqs_to_submit=0
+seq_alias=[]
 dates=[]
 with open (afile_in, "r") as fin, open(gfiletsv_out,"w") as fout1, open(gfasta_out,"w") as fout2, open(linksf_out,"w") as fout3:
     for line in fin:
-        line=line.rstrip()
+        line=line.strip()
         words =line.split("\t")
 
         if line.startswith("Sample"):
             continue
 
-        count +=1
+        seqs_in_access_file +=1
 
         if len(words) != 66:
-            print("check input analysis file at line number " + str(count-1))
+            print("check input access_file at line number " + str(seqs_in_access_file-1))
             continue
 
-        # THIS IS FOR THE FIELD "SequenceSuccess"; aparently no longer used
-        #if words[11] != "TRUE":
-
-        # THIS IS FOR THE FIELD "ToRIVM", Bas uses this to filter sequences for submission
+        # THIS IS FOR THE FIELD "ToRIVM"
         if words[11] != "TRUE":
-            print("skipping line " +str(count))
+            print("skipping line " +str(seqs_in_access_file))
             continue
 
         else:
-            tosubmit+=1
+            seqs_to_submit+=1
 
         sampletype=words[6] #sample type lysis or throat/nasal
         samplequestion=words[8] #sample question surveillance
 
         # words[13] hCov-19/Netherlands/ZH-EMC-7898/02-03-2023
-        first_alias="/".join(words[13].split("/")[1:-1])
+        seq_alias="/".join(words[13].split("/")[1:-1])
 
         # words[7] is sampling date
         dates=words[13].split("/")[-1].split("-")
 
-        # something wrong in the sampling date and the alias from the analysis file
+        # something wrong in the sampling date and the alias from the access_file
         if "/".join(dates) != words[7]:
-            print("sampling date and alias date inconsistent: check line " + str(count-1))
+            print("sampling date and alias date inconsistent: check line " + str(seqs_in_access_file-1))
 
         if len(dates)==3:
             # format the date as per GISAID/ENA requirements
@@ -83,14 +74,14 @@ with open (afile_in, "r") as fin, open(gfiletsv_out,"w") as fout1, open(gfasta_o
             format_date=yyyy+"-"+mm+"-"+dd
 
         else:
-            print("alias date (words[13] 'SequenceID_Internal') has problem: check line " + str(count-1) + "; alias hCoV-19/" + first_alias)
+            print("alias date (words[13] 'SequenceID_Internal') has problem: check line " + str(seqs_in_access_file-1) + "; alias hCoV-19/" + seq_alias)
             format_date=str(2024)
             yyyy="2024"
 
         # words[14] is Viro_run_zzz # words[15] is BByy
         ruzzz_bcyy="Run"+words[14].split("_")[2]+"_"+words[15]
 
-        alias="hCoV-19/"+first_alias+"/"+str(yyyy)
+        alias="hCoV-19/"+seq_alias+"/"+str(yyyy)
 
         #print(format_date+"\t"+ruzzz_bcyy+"\t"+alias+"\t"+sampletype+"\t"+words[64])
         #fout1.write(format_date+"\t"+ruzzz_bcyy+"\t"+alias+"\t"+sampletype+"\t"+words[64]+"\t"+"Europe / Netherlands / "+words[64]+"\n")
@@ -120,28 +111,21 @@ with open (afile_in, "r") as fin, open(gfiletsv_out,"w") as fout1, open(gfasta_o
                 words[64]="Flevoland"
 
         if words[64]=="":
-            print("Province has problem: check line " + str(count-1))
+            print("Province has problem: check line " + str(seqs_in_access_file-1))
 
-        # create parsed file that can be directly copy pasted to the GISAID.xls file
-        fout1.write("GISAID_"+datum+".fasta\t"+alias+"\tbetacoronavirus\tOriginal\t"+format_date+"\t"+"Europe / Netherlands / "+words[64]+"\t"+ruzzz_bcyy+"\t"+sampletype+"\t"+words[64]+"\n")
+        # create file that can be directly used in the GISAID.xls file
+        fout1.write("GISAID_"+submission_date+".fasta\t"+alias+"\tbetacoronavirus\tOriginal\t"+format_date+"\t"+"Europe / Netherlands / "+words[64]+"\t"+ruzzz_bcyy+"\t"+sampletype+"\t"+words[64]+"\n")
 
         # create fasta file to submit to GISAID
         fout2.write(">"+alias+"\n")
         fout2.write(words[18]+"\n")
 
-        # create makelinks file; create symlink_file script
+        # create file to make symlinks to the raw reads
         fout3.write("ln -s /mnt/viro0002/sequencedata/processed/SARS-CoV-2/Runs/"+words[14]+"/filtered/barcode"+words[15][2:]+"_filtered.fastq "+ruzzz_bcyy+".fastq"+"\n")
 
-        #print(len(words[18]))
-        #print(alias)
+print("total samples in access_file: " + str(seqs_in_access_file))
+print("total samples to submit: " + str(seqs_to_submit))
 
-print("total samples in analysis file: " + str(count))
-print("total samples to submit: " + str(tosubmit))
-
-
-
-
-#
 # 0  # SampleID	Supplier	Applicant	SampleEMCCode
 # 4  # SampleExtCode	SampleCoronIT	SampleType	SamplingDate
 # 8  # SampleQuestion	CtValue	SampleRemarks	SequenceSuccess
