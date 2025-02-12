08157278b43371ee6cafed213d91be6c33f6a3b6
Div <28270726+divprasad@users.noreply.github.com>
2025-02-12 15:37:35 +0100

    major edit

--- access_file_parser.py
+++ access_file_parser.py
@@ -3,7 +3,7 @@ import argparse
 # Set up argument parser
 parser = argparse.ArgumentParser(
     prog='submission_parser',
-    description='Parses access_file to generate submission files for GISAID and ENA'
+    description='Extracts and processes sequencing data from an access file to create submission-ready files for GISAID and ENA.'
 )
 
 # Add filename argument
@@ -12,6 +12,13 @@ parser.add_argument('access_file', help="Input file in 'dd-mm-yy.tsv' format")
 # Parse arguments
 args = parser.parse_args()
 access_file_name = args.access_file
+
+PROVINCE_MAP = {
+    "ZH": "Zuid-Holland", "ZE": "Zeeland", "NH": "Noord-Holland",
+    "UT": "Utrecht", "GE": "Gelderland", "NB": "Noord-Brabant",
+    "LI": "Limburg", "OV": "Overijssel", "GR": "Groningen",
+    "DR": "Drenthe", "FL": "Flevoland"
+}
 
 # Extract the submission date from the filename (before '.tsv')
 submission_date = access_file_name.rsplit('.tsv', 1)[0]
@@ -25,10 +32,10 @@ symlink_script_filename = f"makeLinks_{submission_date}.sh"
 total_samples = 0
 samples_for_submission = 0
 
-with open (access_file_name, "r") as access_file_in, \
-    open(gisaid_tsv_filename,"w") as gisaid_tsv_file_out, \
-    open(gisaid_fasta_filename,"w") as gisaid_fasta_out, \
-    open(symlink_script_filename,"w") as symlinks_file_out:
+with open(access_file_name, "r") as access_file_in,        \
+     open(gisaid_tsv_filename,"w") as gisaid_tsv_file_out, \
+     open(gisaid_fasta_filename,"w") as gisaid_fasta_out,  \
+     open(symlink_script_filename,"w") as symlinks_file_out:
     for line in access_file_in:
         line=line.strip()
         words =line.split("\t")
@@ -39,12 +46,12 @@ with open (access_file_name, "r") as access_file_in, \
         total_samples +=1
 
         if len(words) != 66:
-            print("check input access_file at line number " + str(total_samples-1))
+            print("Check input access_file at line number " + str(total_samples-1))
             continue
 
-        # THIS IS FOR THE FIELD "ToRIVM"
+        # this is the fields "ToRIVM"
         if words[11] != "TRUE":
-            print("skipping line " +str(total_samples))
+            print("Skipping line " +str(total_samples))
             continue
 
         else:
@@ -61,18 +68,11 @@ with open (access_file_name, "r") as access_file_in, \
 
         # something wrong in the sampling date and the alias from the access_file
         if "/".join(dates) != words[7]:
-            print("sampling date and alias date inconsistent: check line " + str(total_samples-1))
+            print("Sampling date and date extracted from sample_alias inconsistent: check line " + str(total_samples-1))
 
         if len(dates)==3:
             # format the date as per GISAID/ENA requirements
-            yyyy=dates[2]
-            mm=dates[1]
-            dd=dates[0]
-
-            # check if date is in double digit format
-            if len(dd) != 2:
-                # app
-                dd="0"+dd
+            yyyy, mm, dd=dates[2], dates[1].zfill(2), dates[0].zfill(2)
 
             # check if the barcode is in BC+ double digit format
             if len(words[15]) != 4:
@@ -88,52 +88,37 @@ with open (access_file_name, "r") as access_file_in, \
             yyyy="2024"
 
         # words[14] is Viro_run_zzz # words[15] is BByy
-        ruzzz_bcyy="Run"+words[14].split("_")[2]+"_"+words[15]
+        runyy_bczz="Run"+words[14].split("_")[2]+"_"+words[15]
 
         alias="hCoV-19/"+seq_alias+"/"+str(yyyy)
 
-        #print(format_date+"\t"+ruzzz_bcyy+"\t"+alias+"\t"+sampletype+"\t"+words[64])
-        #gisaid_tsv_file_out.write(format_date+"\t"+ruzzz_bcyy+"\t"+alias+"\t"+sampletype+"\t"+words[64]+"\t"+"Europe / Netherlands / "+words[64]+"\n")
 
-        if words[64]=="":
-            if words[61]=="ZH":
-                words[64]="Zuid-Holland"
-            elif words[61]=="ZE":
-                words[64]="Zeeland"
-            elif words[61]=="NH":
-                words[64]="Noord-Holland"
-            elif words[61]=="UT":
-                words[64]="Utrecht"
-            elif words[61]=="GE":
-                words[64]="Gelderland"
-            elif words[61]=="NB":
-                words[64]="Noord-Brabant"
-            elif words[61]=="LI":
-                words[64]="Limburg"
-            elif words[61]=="OV":
-                words[64]="Overijssel"
-            elif words[61]=="GR":
-                words[64]="Groningen"
-            elif words[61]=="DR":
-                words[64]="Drenthe"
-            elif words[61]=="FL":
-                words[64]="Flevoland"
-
-        if words[64]=="":
-            print("Province has problem: check line " + str(total_samples-1))
+        province = words[64] or PROVINCE_MAP.get(words[61], "")
+        if not province:
+            print(f"Province has problem: check line {total_samples}")
 
         # create file that can be directly used in the GISAID.xls file
-        gisaid_tsv_file_out.write("GISAID_"+submission_date+".fasta\t"+alias+"\tbetacoronavirus\tOriginal\t"+format_date+"\t"+"Europe / Netherlands / "+words[64]+"\t"+ruzzz_bcyy+"\t"+sampletype+"\t"+words[64]+"\n")
+        gisaid_tsv_file_out.write(
+            f"GISAID_{submission_date}.fasta\t{alias}\t"
+            f"betacoronavirus\tOriginal\t{format_date}\t"
+            f"Europe / Netherlands / {province}\t"
+            f"{runyy_bczz}\t{sampletype}\t"
+            f"{province}\n"
+        )
 
         # create fasta file to submit to GISAID
-        gisaid_fasta_out.write(">"+alias+"\n")
-        gisaid_fasta_out.write(words[18]+"\n")
+        gisaid_fasta_out.write(f">{alias}\n{words[18]}\n")
 
         # create file to make symlinks to the raw reads
-        symlinks_file_out.write("ln -s /mnt/viro0002/sequencedata/processed/SARS-CoV-2/Runs/"+words[14]+"/filtered/barcode"+words[15][2:]+"_filtered.fastq "+ruzzz_bcyy+".fastq"+"\n")
+        symlinks_file_out.write(
+            f"ln -s /mnt/viro0002/sequencedata/processed/SARS-CoV-2/Runs/"
+            f"{words[14]}/filtered/barcode{words[15][2:]}_filtered.fastq "
+            f"{runyy_bczz}.fastq\n"
+        )
 
-print("total samples in access_file: " + str(total_samples))
-print("total samples to submit: " + str(samples_for_submission))
+
+print(f"Total samples in access_file: {total_samples}")
+print(f"Total samples to submit: {samples_for_submission}")
 
 # 0  # SampleID	Supplier	Applicant	SampleEMCCode
 # 4  # SampleExtCode	SampleCoronIT	SampleType	SamplingDate
